014ffb5ae103d151df71e5b6500f94271bccd806
ohm-vishwa <ohmdto1513@gmail.com>
2026-07-03 17:23:08 +0530

    feat: add server and preview configuration with allowed hosts in vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,21 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: [
+      "resume-analyzer-zcke.onrender.com",
+      "full-stack-gen-ai-resume-frontend.onrender.com",
+      "full-stack-gen-ai-resume-analyzer.onrender.com",
+    ],
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 4173,
+    allowedHosts: [
+      "resume-analyzer-zcke.onrender.com",
+      "full-stack-gen-ai-resume-frontend.onrender.com",
+      "full-stack-gen-ai-resume-analyzer.onrender.com",
+    ],
+  },
 });
